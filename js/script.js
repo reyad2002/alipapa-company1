cad31a308dd3ed94a1b5c9d2b1340551544178d6
Reyad Mohamed Fathy <112622902+reyad2002@users.noreply.github.com>
2023-12-08 18:10:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,132 @@
+// document.getElementById("btn1").onclick= function (){
+//     document.getElementById("reyad").innerHTML=("reyad")
+// }
+//////////////////////////////////////////////////////////
+// var x=120
+// var y=210
+// var z=["ahmed","reyad","mona","rawda","mohamed"]
+//  console.log(typeof x) // typeof <------علشان اعرف ال data type
+// console.log(z[3])
+// console.log(typeof z[2])
+//////////////////////////////////////////////////////////
+
+//console.log(x++)    //مش هيضيف واحد في الطباعة بس قيمة الاضافة هتتخزن
+//console.log(++x)    //بيضيف القيمة ويطبعها
+//////////////////////////////////////////////////////////
+
+//arthematic operators  -  +  *  /  %    ++  --  **
+
+//assignment operators  +=  -=  *=  /=
+// x=x+30     x+=30
+// console.log(x)
+
+// to create a funtion
+// function reyad(x,y){
+//     console.log(x+y)
+// }
+// علشان انفذ الامر
+// reyad(50,200)
+
+// if condition
+// var deg =prompt("enter your degree")
+// if(deg>=85){
+// console.log("exclent")
+// }
+// else if(deg>=75){
+//     console.log("very good")
+
+// }
+// else if(deg>=65){
+//     console.log(" good")
+
+// }
+// else if(deg>=50){
+//     console.log("sucess")
+
+// }
+// else{
+//     console.log("fial")
+// }
+
+/////////////////////////
+// var number= prompt("enter number")
+// for( x=0;
+//      x<=number;
+//      x++ ){
+//         console.log(x)
+//      }
+
+///////////////////////////////////
+
+// var salary = prompt("enter your salary")
+
+// console.log(salary/=2)
+
+//////////////////////////////
+// var salary = prompt("enter your salary")
+// if(salary>=17000){
+//  console.log("Wealthy")
+// }
+// else if(salary>=9000){
+//     console.log("rich")
+// }
+// else{
+//     console.log("mashia be elsatr")
+// }
+// var salary = prompt("enter your salary")
+// if(salary>=17000){
+//  document.getElementById("reyad").innerHTML="welthy"
+// }
+// else if(salary>=9000){
+//     document.getElementById("reyad").innerHTML="rich"
+// }
+// else{
+//     document.getElementById("reyad").innerHTML="على الله"
+// }
+
+// var btn1 = document.getElementById("btn1")
+// var btn2 = document.getElementById("btn2")
+// var btn3 = document.getElementById("btn3")
+// var btn4 = document.getElementById("btn4")
+// var btn5 = document.getElementById("btn5")
+// var btn6 = document.getElementById("btn6")
+// var btn7 = document.getElementById("btn7")
+// var btn8 = document.getElementById("btn8")
+// var sum = document.getElementById("total")
+// btn1.addEventListener("click", sum1)
+// btn2.addEventListener("click", sum2)
+// btn3.addEventListener("click", sum3)
+// btn4.addEventListener("click", sum4)
+// btn5.addEventListener("click", sum5)
+// btn6.addEventListener("click", sum6)
+// btn7.addEventListener("click", sum7)
+// btn8.addEventListener("click", sum8)
+
+// function sum1(){
+//  sum.innerHTML="100"
+// }
+// function sum2(){
+//  sum.innerHTML="200"
+// }
+// function sum3(){
+//  sum.innerHTML="300"
+// }
+// function sum4(){
+//  sum.innerHTML="400"
+// }
+// function sum5(){
+//  sum.innerHTML="500"
+// }
+// function sum6(){
+//  sum.innerHTML="600"
+// }
+// function sum7(){
+//  sum.innerHTML="700"
+// }
+// function sum8(){
+//  sum.innerHTML="800"
+// }
+
+//    var x=20
+//    var y =50
+//     console.log(x+y)
